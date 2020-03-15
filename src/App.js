3b2ae7f03647ab649cf35d3558911f1e58dd0e55
Sai Sankeerth <sankeerth2292@gmail.com>
2020-03-15 23:36:33 +0530

    Header component added & styling changes for a & also padding adjusted for logo for center-aligning the header & a components commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact component={HomePage} path='/' />
         <Route exact component={ShopPage} path='/shop/' />
