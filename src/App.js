a6dde0495dc00ec5e2872bab105c920fdce31b67
Sai Sankeerth <sankeerth2292@gmail.com>
2020-03-15 23:03:22 +0530

    Shop Page, CollectionPreview component creation & added routes to them

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
+import ShopPage from './pages/shop/shop';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact component={HomePage} path='/' />
-        <Route exact component={HatsPage} path='/shop/hats' />
+        <Route exact component={ShopPage} path='/shop/' />
       </Switch>
     </div>
   );
