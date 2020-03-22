df67a8ee5d2cad2a50f3aa60b3620380bffc4a6f
Sai Sankeerth <sankeerth2292@gmail.com>
2020-03-22 10:56:13 +0530

    SignUp with e-mail and password implementation

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ class App extends React.Component {
               id: snapshot.id,
               ...snapshot.data()
             }
+          }, () => {
+            console.log(this.state);
           });
         });
       } else {
--- src/pages/sign-in-sign-up/sign-in-sign-up.js
+++ src/pages/sign-in-sign-up/sign-in-sign-up.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in';
 import './sign-in-sign-up.scss';
+import SignUp from '../../components/sign-up/sing-up';
 
 const SignInSignUpPage = () => {
-    return <div className='sign-in-and-sign-up'><SignIn /></div>
+    return (
+        <div className='sign-in-and-sign-up'>
+            <SignIn />
+            <SignUp />
+        </div>
+    );
 };
 
 export default SignInSignUpPage;
