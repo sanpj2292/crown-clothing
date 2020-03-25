806bb863b53f39aad44da69c1e99ed6609c88b5b
Sai Sankeerth <sankeerth2292@gmail.com>
2020-03-25 17:26:29 +0530

    Displaying added items into CartDropdown component and changing the style of the scroll-bar for all inside this app

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { connect } from 'react-redux';
 import CustomButton from "../custom-button/custom-button";
+import CartItem from "../cart-item/cart-item";
+
 import "./cart-dropdown.scss";
 
-const Cart = () => {
+const Cart = ({ cartItems }) => {
     return (
         <div className='cart-dropdown'>
-            <div className='cart-items'></div>
+            <div className='cart-items'>
+                {
+                    cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+
+                }
+            </div>
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     )
 
 };
 
-export default Cart;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+});
+
+export default connect(mapStateToProps)(Cart);
