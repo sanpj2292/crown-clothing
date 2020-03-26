68528075691192c48aeb5c8cd2cedea103dfc345
Sai Sankeerth <sankeerth2292@gmail.com>
2020-03-26 13:49:36 +0530

    Reorganizing the imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-
 import { Provider } from 'react-redux';
 import { PersistGate } from "redux-persist/integration/react";
 
+import './index.css';
+import App from './App';
 import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
