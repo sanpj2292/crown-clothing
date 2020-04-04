d77f113a31fb49f7d73e61a4a7feeb7e4d8258ce
Sai Sankeerth <sankeerth2292@gmail.com>
2020-04-04 19:54:54 +0530

    Post insertion deletion commit

--- src/App.js
+++ src/App.js
@@ -10,10 +10,9 @@ import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up';
 import CheckoutPage from "./pages/checkout/checkout";
 
 import Header from './components/header/header';
-import { auth, createUserProfileDocument, addCollectionDocs } from './firebase/firebase-utils';
+import { auth, createUserProfileDocument } from './firebase/firebase-utils';
 import { setCurrentUser } from "./redux/user/user-actions";
 import { selectCurrentUser } from "./redux/user/user-selectors";
-import { selectCollectionsForPreview } from "./redux/shop/shop-selectors";
 
 class App extends React.Component {
 
@@ -22,7 +21,7 @@ class App extends React.Component {
 
   componentDidMount() {
     // This setCurrentUser is different from the one imported
-    const { setCurrentUser, collectionArray } = this.props;
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
@@ -35,7 +34,6 @@ class App extends React.Component {
       } else {
         setCurrentUser(userAuth);
       }
-      addCollectionDocs('collections', collectionArray.map(({ title, items }) => ({ title, items })));
     });
   }
 
@@ -67,7 +65,6 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-  collectionArray: selectCollectionsForPreview
 });
 
 
